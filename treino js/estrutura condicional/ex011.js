3d8bc369ada0fc2dc268b126c52d1ae37f12dc40
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-01-18 06:29:53 -0300

    1.5

---
+++ treino js/estrutura condicional/ex011.js
@@ -0,0 +1,9 @@
+var idade = 69
+console.log(`você tem ${idade} anos.`)
+if(idade < 16){
+    console.log('não vota')
+} else if (idade >= 16 && idade < 18 || idade >= 65){
+    console.log('voto opcional')
+}else {
+    console.log('voto obrigatório')
+}
