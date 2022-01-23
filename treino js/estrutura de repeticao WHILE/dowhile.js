3107264e7f796f9ef271c46cdd4ae2ab91ae1c95
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-01-23 07:48:17 -0300

    2.15

---
+++ treino js/estrutura de repeticao WHILE/dowhile.js
@@ -0,0 +1,5 @@
+var c = 1    //teste lógico no final
+do{
+    console.log(`Passo ${c}`)
+    c++
+}while(c <= 6)
---
+++ treino js/estrutura de repeticao WHILE/while.js
@@ -0,0 +1,5 @@
+var c = 1    //teste lógico no inicio
+while (c <= 106){  //Enquanto c for menor que 6 faça:
+    console.log(`Passo ${c} vezes.`)
+    c++     //c = c + 1
+}
