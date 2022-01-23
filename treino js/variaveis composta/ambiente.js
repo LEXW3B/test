7cf5c1ab5b2bbb622fe7ecd37244590621a5551b
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-01-23 20:39:52 -0300

    nova

--- treino js/variaveis composta/ambiente.js
+++ treino js/variaveis composta/ambiente.js
@@ -8,9 +8,9 @@ num.push(1)
 num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
+let pos = num.indexOf(8)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+console.log(`O valor está na posição ${pos}`)
 
-for(let pos = 0; pos < num.length; pos++){
-    console.log(num[pos])
-}
+
 
---
+++ treino js/variaveis composta/forIn.js
@@ -0,0 +1,6 @@
+
+let valores = [1,2,3,4,5,6]
+for(let pos in valores){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+
