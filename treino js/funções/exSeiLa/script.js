d8b3828351c89a2d7665864a5c78f6b226a1aefc
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-01 12:36:02 -0300

    analisador de números 1.1

--- treino js/funções/exSeiLa/script.js
+++ treino js/funções/exSeiLa/script.js
@@ -27,16 +27,48 @@ function inLista(n, l){
 //criar funcoes para isNumero=número e isLista=lista
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
-        window.alert('Tudo ok')
+        //adiciona o numero dentro da biblioteca valores
+        valores.push(Number(num.value))
+        //faz aparecer visualmente dwentro do select
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ' '
     }else{
         window.alert('Valor inválido ou já encontrado na lista.')
     }
-
-
-
-
+    num.value = ' '
+    num.focus()
 }
-
+//criar as funções FINALIZAR
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar!')
+    }else{
+        let tot = valores.length
+        //criar comparação de maior e menor número
+        let maior = valores[0]
+        let menor = valores[0]
+        //aproveitar este codigo e fazer a soma e média também
+        let soma = 0 
+        let media = 0 
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior){
+                maior = valores[pos]
+            }if(valores[pos] < menor){
+                menor = valores[pos]
+            }
+        }
+        media = soma/tot
+        res.innerHTML = ' '
+        res.innerHTML += `<p>Ao todo, temos <strong>${tot}</strong> números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi <strong>${maior}</strong>.</p>`
+        res.innerHTML += `<p>O menor valor informado foi <strong>${menor}</strong>.</p>`
+        res.innerHTML += `<p>Somando todos os valores temos <strong>${soma}</strong>.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é <strong>${media}</strong>.</p>`
+    }
+}
 
 
 
