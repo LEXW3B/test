2218ed2581784900befb56a8780b2d0fb750b051
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-01 06:28:14 -0300

    13

---
+++ treino js/funções/parImpar.js
@@ -0,0 +1,15 @@
+// função de par ou impar
+
+function parimp(n){
+    if(n % 2 == 0){
+        return 'Par'
+    }else{
+        return 'Impar'
+    }
+}
+let resp = parimp(12)
+console.log(resp)
+
+
+
+
