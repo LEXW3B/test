b30c81ae4dc9ed11fd9d9be740efa9202b7c5648
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-01 18:42:45 -0300

    tipos

---
+++ novo treino js/variaveis/tipos_primitivos.js
@@ -0,0 +1,29 @@
+//string,         number=(int, floats),         boolean=(true,false)
+
+//string
+let myVar = 'minha string com aspas simples'
+let myVar2 = "minha string com aspas duplas"
+var myVar3 = `minha template literal`
+//concatenação
+let idade = 40
+let msg = "eu possuo " + idade + " anos."
+//interpolação ou interpola // usa o acrase
+let id = 38
+let mensg = `eu possuo ${id} idade anos.`
+
+
+//number
+const n1 = 10
+const n2 = 1.1
+console.log(`o tipo de n1 é ${typeof n1} e seu valor é ${n1}`)
+console.log(`o tipo de n2 é ${typeof n2} e seu valor é ${n2}`)
+
+
+//boolean
+
+const isValid = false
+console.log(`isValid: ${isValid}`)
+
+
+
+
