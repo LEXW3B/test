c5a92772b20b96280eafc0ca7e6b41ef929ee9e2
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-01 18:51:37 -0300

    tipos 1.1

---
+++ novo treino js/variaveis/tipos_prim_cont.js
@@ -0,0 +1,20 @@
+//undefined,        null,         symbol
+
+//undefined, porque não atribuiu valor a variavel
+let varTest
+console.log(varTest)
+console.log(typeof varTest)
+
+//Null
+let varTest1 = null
+console.log(typeof varTest1, varTest1)
+
+
+
+
+
+
+
+
+
+
