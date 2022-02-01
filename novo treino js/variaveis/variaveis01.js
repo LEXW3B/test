8ddb0bca41ef16c17fcf40527610c83cd8dc4ea4
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-01 18:05:10 -0300

    variaveis01

---
+++ novo treino js/variaveis/variaveis01.js
@@ -0,0 +1,24 @@
+// var, let, const
+
+let teste = 'minha string'//let pode ser reatribuido depois
+teste = 100//reatribuição isso deixa o js sendo fracamente tipado
+
+
+const teste2 = 10;//const não permite reatribuição
+//teste2 = 11; da erro não permite reatribução
+console.log(teste2)
+
+let teste3;
+teste3 = 20;
+console.log(teste3)
+
+const teste4 = 'eu sou imutavel'
+console.log(teste4)
+
+
+
+
+
+
+
+
