1a426db4cfd773c63f44d8a7b26218b9c0d3ab5a
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-02 15:56:23 -0300

    operadores 1.0

--- novo treino js/variaveis/operadores aritimeticos/operadores.js
+++ novo treino js/variaveis/operadores aritimeticos/operadores.js
@@ -1,4 +1,5 @@
 // +  -  *  /  %  **
+//OPERADORES ARITIMETICOS
 let n1 = 10;
 let n2 = 5;
 console.log(n1 +  n2)
@@ -8,11 +9,12 @@ console.log(n1 /  n2)
 console.log(n1 %  n2)
 console.log( 2 ** 3 )
 
-
-
-
-
-
+//OPERADORES DE ATRIBUIÇÃO
+let n3 = 20;
+//n3 = n3 * 2  ou
+//n3 *= 2;
+n3 %= 2  //resto da divisão
+console.log(n3)
 
 
 
