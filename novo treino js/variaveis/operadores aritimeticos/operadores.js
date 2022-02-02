9a446fdcf4390a948797f7099148bb43a0ca67b5
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-02 15:47:16 -0300

    operadores

---
+++ novo treino js/variaveis/operadores aritimeticos/operadores.js
@@ -0,0 +1,22 @@
+// +  -  *  /  %  **
+let n1 = 10;
+let n2 = 5;
+console.log(n1 +  n2)
+console.log(n1 -  n2)
+console.log(n1 *  n2)
+console.log(n1 /  n2)
+console.log(n1 %  n2)
+console.log( 2 ** 3 )
+
+
+
+
+
+
+
+
+
+
+
+
+
