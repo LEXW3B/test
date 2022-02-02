23213aa54df5db26aa085b2faddd00bc69f44643
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-02 15:40:30 -0300

    conversao

--- novo treino js/variaveis/conversao_tipos.js
+++ novo treino js/variaveis/conversao_tipos.js
@@ -4,13 +4,20 @@ let n2 = '2.9999'
 n2 = parseFloat(n2)
 
 
-console.log(n1 + n2, typeof n1, typeof n2, n2)
+//console.log(n1 + n2, typeof n1, typeof n2, n2)
+
+//metodo para transformar numero em string "toString()"
+n2 = 11
+n2 = n2.toString()
+//console.log(n2, typeof n2)
 
 
-
-
-
-
+///////////////////////////////////////////////////
+//tranformação para numeros hexadecimal
+n5 = 12548
+n5 = n5.toString(15)
+console.log(n5)
+///////////////////////////////////////////////////
 
 
 
