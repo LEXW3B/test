bce96fa8f038e7c9450dabdff82fe5d46f9598ce
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-05 12:47:12 -0300

    ifelse

---
+++ novo treino js/condições/CeV/if-else/ex001.js
@@ -0,0 +1,19 @@
+
+let vel = 70.5
+console.log(`A velocidade do seu carro e de ${vel} Km/h.`)
+if(vel > 60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!!!`)
+}
+console.log(`Dirija sempre com o cinto de segurança`)
+
+
+
+
+
+
+
+
+
+
+
+
