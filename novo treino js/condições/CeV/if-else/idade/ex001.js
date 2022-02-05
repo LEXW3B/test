b3ccab6b0a6ec92bfd007d0b00a39e60a3078a75
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-05 20:01:10 -0300

    ex

---
+++ novo treino js/condições/CeV/if-else/idade/ex001.js
@@ -0,0 +1,17 @@
+let idade = 22;
+if(idade<16){
+    console.log(`Não vota.`)
+}else if(idade < 18 || idade > 65){
+    console.log(`Voto opcional.`)
+}else{
+    console.log(`Voto obrigatório.`)
+}
+
+
+
+
+
+
+
+
+
