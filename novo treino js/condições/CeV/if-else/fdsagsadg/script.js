39536fb3aa4fa16c2e7f4c76df358ef783bd1687
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-06 09:49:39 -0300

    dia

---
+++ novo treino js/condições/CeV/if-else/fdsagsadg/script.js
@@ -0,0 +1,36 @@
+
+function carregar() {
+    "use strict";
+    let msg = document.getElementById('msg')
+    let img = document.getElementById('imagem')
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if(hora>=0 && hora<12){
+        img.src = 'img/manha.png'
+        document.body.style.background='#e2cd9f'
+    }else if(hora>=12 && hora<=18){
+        img.src = 'img/tarde.png'
+        document.body.style.background='#b9846f'
+    }else{
+        img.src = 'img/noite.png'
+        document.body.style.background='#515154'
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
