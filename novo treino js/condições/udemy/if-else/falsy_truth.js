258c2a53c59bd2ab5c038f6d6e7b741c95e8e3aa
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-07 11:11:17 -0300

    verdade ou false

---
+++ novo treino js/condições/udemy/if-else/falsy_truth.js
@@ -0,0 +1,21 @@
+//falsy values: 0, "" , NaN, undedfined, null, false;
+//truthy: todos os demais;
+if("false"){
+    console.log('true')
+}else{
+    console.log('false')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
