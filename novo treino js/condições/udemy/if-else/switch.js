edc240d0e5a1193d5a3e1f876a75fb73435f54f6
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-07 13:28:39 -0300

    switch

---
+++ novo treino js/condições/udemy/if-else/switch.js
@@ -0,0 +1,42 @@
+//estrutura switch
+let diaSemana = 6
+let dia = ''
+switch(diaSemana){
+    case 0:
+        dia='domingo'
+        break
+    case 1:
+        dia='Segunda'
+        break
+    case 2:
+        dia='terça'
+        break
+    case 3:
+        dia='quarta'
+        break
+    case 4:
+        dia='quinta'
+        break
+    case 5:
+        dia='sexta'
+        break
+    case 6:
+        dia='sábado'   
+        break
+    default:
+        dia=' -- ' 
+}
+console.log(dia)
+
+
+
+
+
+
+
+
+
+
+
+
+
