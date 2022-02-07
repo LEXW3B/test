e5098dfb9c0bebe564cdc95b8e08bdaa1da20ffc
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-07 11:39:27 -0300

    CURTO CIRCUTO

---
+++ novo treino js/condições/udemy/if-else/curto_circuito.js
@@ -0,0 +1,30 @@
+
+//Curto circuito
+let n = 0
+n=n||10
+console.log(n)
+
+let isValid = false
+//isValid&&console.log('É valido')
+isValid||console.log('É válido')
+
+//usando método ternário
+let ternário = (n===0)?`false`:`thuthy`
+console.log(ternário)
+
+//Estrutura if else
+// if(n===0){
+//     console.log(`False`)
+// }else{
+//     console.log(`Truthy`)
+// }
+
+
+
+
+
+
+
+
+
+
