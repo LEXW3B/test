9542c06df26ea6c19fd50df546d9a70ff4645c8d
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-08 18:48:16 -0300

    FOR

---
+++ novo treino js/repetições/CeV/for/repeticaoFor.js
@@ -0,0 +1,18 @@
+
+console.log('vai começar....')
+for (let c = 1; c<=3;c++){
+    console.log(c)
+}
+console.log('FIM!')
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ novo treino js/repetições/CeV/while/repeticaoWhile.js
@@ -0,0 +1,32 @@
+//repetição while - quando já tem definido quantas vezes vai querer repetir.
+//while com teste lógico no início
+let contador = 1
+while(contador<=6){
+    console.log(`passo: ${contador}`)
+    contador++
+}
+//do while - teste lógico no final
+console.log('-=-=-==-=-=-')
+let cu = 1
+do{
+    console.log(`Passo: ${c}`)
+    c++
+}while(c<=6)
+
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+// console.log('tudo bem?')
+console.log('-=-===-=-=-=-=-')
+
+
+
+
+
+
+
+
+
