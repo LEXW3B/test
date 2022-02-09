7b426324d629c76eb9848255f631f9165224e8fb
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-09 14:29:33 -0300

    array

---
+++ novo treino js/ARRAY/CeV/array.js
@@ -0,0 +1,19 @@
+
+let num = [5, 8, 2, 9, 3]
+
+num[5] = 1 //diz em qual posição deve inserir o novo numero
+num.push(7)//acrescenta novo numero sempre na ultima posição
+num.length //diz o cumprimento da varievel
+//num.sort()//pega todos os valores da array e reposiciona em ordem crescente
+
+console.log(`esta e minha array: ${num}`)
+console.log(`o cumprimento da minha array e: ${num.length}`)
+console.log(`o primeiro valor da minha array e: ${num[0]}`)
+
+
+
+
+
+
+
+
