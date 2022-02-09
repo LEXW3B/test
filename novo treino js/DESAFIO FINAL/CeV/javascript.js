eb1abbd5445c96318e4c436828ad9850e99b5f46
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-09 18:50:47 -0300

    teste

---
+++ novo treino js/DESAFIO FINAL/CeV/javascript.js
@@ -0,0 +1,51 @@
+
+let numero = document.querySelector('#txtn')
+let tela = document.querySelector('#tela')
+let final = document.querySelector('#finalizar')
+let resp = document.querySelector('#resp')
+let valores = []
+    
+function eNumero (numero) {
+    if (Number(numero) >= 1 && Number(numero) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}    
+    
+function eLista () {
+    if (tela.indexOf(Number())) {
+        return
+    } else {
+        return
+    }
+}
+
+
+
+function analizar () {
+        
+    
+}
+if (numero.value.length == 0) {
+    window.alert('Digite um número no campo abaixo.')       
+    
+} else if () {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
