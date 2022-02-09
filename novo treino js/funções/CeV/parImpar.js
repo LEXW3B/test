7eaa4e929de285f47a181713c32e5804a82be63e
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-09 16:46:48 -0300

    funcao

---
+++ novo treino js/funções/CeV/parImpar.js
@@ -0,0 +1,20 @@
+
+function parImpar (n) {
+    
+    if (n % 2 == 0) {
+        return 'Par'
+    } else {
+        return 'Impar'
+    }
+}
+
+let resp = parImpar(10)
+console.log(resp)
+
+
+
+
+
+
+
+
