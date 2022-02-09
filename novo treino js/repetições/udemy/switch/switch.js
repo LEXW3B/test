1994ae963800f421d37ea3d8f6de88cce74334dd
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-09 10:54:07 -0300

    switch

---
+++ novo treino js/repetições/udemy/switch/switch.js
@@ -0,0 +1,29 @@
+//estrutura switch
+let diaSemana = 3
+let dia = ''
+switch (diaSemana) {
+    case 0:
+        dia = 'Domingo'
+        break
+    case 1:
+        dia = 'Segunda'
+        break
+    case 2: 
+        dia = 'Terça'
+        break
+    case 3:
+        dia = 'Quarta'
+        break
+    case 4:
+        dia = 'Quinta'
+        break
+    case 5:
+        dia = 'Sexta'
+        break
+    case 6:
+        dia = 'Sábado'
+        break
+    default:
+        dia = ' -=- '
+}
+console.log(`Hoje é ${dia}`)
