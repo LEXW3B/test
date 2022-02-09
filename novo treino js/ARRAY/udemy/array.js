1d75421bf674669fb368084af3ba9ebca2f59fbc
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-09 15:48:43 -0300

    arr

---
+++ novo treino js/ARRAY/udemy/array.js
@@ -0,0 +1,15 @@
+
+const arr = new Array()
+const arr2 = new Array(true, 'Alex', 39, new Array(2, 4, 10))
+arr[0] = 'Alex'
+arr[1] = 40
+console.log(arr)
+console.log(arr2[3].length)
+
+
+
+
+
+
+
+
