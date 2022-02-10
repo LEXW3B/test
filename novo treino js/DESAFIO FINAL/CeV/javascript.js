d0f071f5b107e418949a6433f6f959ef5681f7b7
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-10 08:54:48 -0300

    finalizado

--- novo treino js/DESAFIO FINAL/CeV/javascript.js
+++ novo treino js/DESAFIO FINAL/CeV/javascript.js
@@ -1,51 +1,65 @@
 
-let numero = document.querySelector('#txtn')
-let tela = document.querySelector('#tela')
-let final = document.querySelector('#finalizar')
+let num = document.querySelector('#fnum')
+let lista = document.querySelector('#flista')
 let resp = document.querySelector('#resp')
 let valores = []
+
+//let final = document.querySelector('#finalizar')
     
-function eNumero (numero) {
-    if (Number(numero) >= 1 && Number(numero) <= 100) {
+function isNumero (n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
         return true
     } else {
         return false
     }
 }    
     
-function eLista () {
-    if (tela.indexOf(Number())) {
-        return
+function isLista (n, l) {
+    if (l.indexOf(Number(n))!= - 1) {
+        return true
     } else {
-        return
+        return false
     }
 }
 
-
-
-function analizar () {
-        
-    
+function adicionar () {
+    if (isNumero(num.value) && !isLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        resp.innerHTML = ' '
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }     
+    num.value = ''
+    num.focus()
 }
-if (numero.value.length == 0) {
-    window.alert('Digite um número no campo abaixo.')       
-    
-} else if () {
-    
+
+function finalizar () {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar.')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        //vai análisar qual número e maior e ou menor.
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            } if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / tot
+        resp.innerHTML = ' '
+        resp.innerHTML += `<p>Ao todo temos ${tot} números cadastrados.</p>`
+        resp.innerHTML += `<p>O maior número informado foi ${maior}.</p>`
+        resp.innerHTML += `<p>O menor número informado foi ${menor}.</p>`
+        resp.innerHTML += `<p>A soma entre todos os valores, temos ${soma}.</p>`
+        resp.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
