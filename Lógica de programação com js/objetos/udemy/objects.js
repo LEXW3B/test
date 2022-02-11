e0f858b1de14e15f5e3a5882fc03749dbc912724
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-11 17:20:08 -0300

    obj

---
+++ Lógica de programação com js/objetos/udemy/objects.js
@@ -0,0 +1,28 @@
+// const nomes = ['daniel', 'maria', 'joao']
+// const idade = [40, 28, 35]
+// console.log(nomes[0], idade[0])
+//sintaxe formal
+const pessoa = new Object()
+pessoa.nome = 'daniel'
+pessoa.idade = 40
+console.log (pessoa)
+//sintaxe literal
+const individuo = {
+    nome: 'Alexandre',
+    idade: 39
+}
+console.log(individuo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
