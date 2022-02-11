c51de31082371e2783786c3ee60d3c7f0fa80690
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-11 17:03:23 -0300

    arr

--- Lógica de programação com js/ARRAY/udemy/array.js
+++ Lógica de programação com js/ARRAY/udemy/array.js
@@ -3,11 +3,13 @@ const arr = new Array()
 const arr2 = new Array(true, 'Alex', 39, new Array(2, 4, 10))
 arr[0] = 'Alex'
 arr[1] = 40
-console.log(arr)
 console.log(arr2[3].length)
-
-
-
+arr2.push('A...')
+let n = 56
+arr2.push(n.value)
+console.log(arr)
+console.log(arr2)
+console.log(arr2[n])
 
 
 
