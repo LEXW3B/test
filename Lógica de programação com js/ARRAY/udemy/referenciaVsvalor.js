d3c896dfea490c75d17339d87d4a959c093810ba
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-12 07:15:19 -0300

    referencia

---
+++ Lógica de programação com js/ARRAY/udemy/referenciaVsvalor.js
@@ -0,0 +1,24 @@
+
+
+function alteraArray(a){
+    arr.push('adicionado')
+}
+let arr = ['a']
+console.log(arr)
+alteraArray(arr)
+console.log(arr)
+
+function alteraPrimitivo (p) {
+    p += ' adicionado'
+    console.log('Dentro da função: ', p)
+}
+let msg = 'mensagem'
+console.log(msg)
+alteraPrimitivo(msg)
+console.log(msg)
+
+
+
+
+
+
