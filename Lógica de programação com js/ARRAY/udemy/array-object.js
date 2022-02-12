3873d16a977a7b758e7b1496386eaeb9079290e3
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-12 06:50:03 -0300

    arr-obj

---
+++ Lógica de programação com js/ARRAY/udemy/array-object.js
@@ -0,0 +1,35 @@
+
+const nomes = ['Daniel', 'Maria', '']
+const pessoa = {nome: 'Daniel', idade: 10, email: ''}
+const pessoa1 = {}
+
+const pessoas = [{
+    nome: 'Alexandre',
+    idade: 39
+}, {
+    nome: 'Daniel',
+    idade: 40
+}, {
+    nome: 'kelly',
+    idade: 28
+}, {
+    nome: 'vanda',
+    idade: 55
+}]
+
+for(let c = 0; c < pessoas.length; c++){
+    console.log(`${pessoas[c].nome} e tenho ${pessoas[c].idade} anos.` )
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
