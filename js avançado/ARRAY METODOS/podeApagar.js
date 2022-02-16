73a9382d547aa7eaaaa3638138fba1833e0e94a8
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-16 20:20:17 -0300

    jjjj

---
+++ js avançado/ARRAY METODOS/podeApagar.js
@@ -0,0 +1,77 @@
+
+
+// let ar = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+
+// function aVeryBigSum(ar){
+//     let total = 0
+
+//     for(let c = 0; c < ar.length; c++) {
+//         total = total + ar[c] 
+//         //return total
+//         console.log(total)
+//     }    
+// }
+
+
+// let result = list.map(number => number*10);
+// console.log(result);
+
+
+// let arr = [
+//     [11,  2,  4],
+//     [4,   5,  6],
+//     [10,  8,-12]
+// ]
+// function diagonalDifference(arr) {
+
+//     let d1 = arr[0][0] 
+//     let d2 = arr[1][1]
+//     let d3 = arr[2][2]
+//     let direita = d1+d2-(d3*(-1))
+
+//     let e1 = arr[0][2]
+//     let e2 = arr[1][1]
+//     let e3 = arr[2][0]
+
+//     let esquerda = e1+e2+e3
+//     let INTEGER_ARRAY =  direita-esquerda
+
+//     return INTEGER_ARRAY * (-1)
+    
+    
+// }
+
+// let s = '07:05:45PM'
+
+// let ss = s.getHours()
+// console.log(ss)
+
+// let s = 'saveChangesInTheEditor'
+
+// function camelcase(s) {
+//     // Write your code here
+//     let STRING = 0
+//     for(let c = 0; c < s.length.toUpperCase; c++){
+//         if(c == s.length.toUpperCase){
+//             STRING = STRING + s[c]
+//         }else{
+//             return 0
+//         }
+//     }
+//     console.log(STRING)
+// }
+
+// let n = 11
+// let password = '#HackerRank'
+
+// let INTEGER = n
+// let STRING = ''
+// for(let c = 0; c < password.length; c++) {
+//     STRING = [c]    
+// }   
+// console.log(INTEGER, STRING.length) 
+
+
+
+
+
